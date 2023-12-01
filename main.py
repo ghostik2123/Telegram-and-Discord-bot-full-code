3a32973e7023ee272b8c3adeba2a177c64c8f933
ghostik23 <87764981+ghostik2123@users.noreply.github.com>
2023-12-01 21:26:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import threading
 import telebot
 from telebot import types
 from datetime import datetime
+import pytz
 import discord
 from discord.ext import commands
+from cryptography.fernet import Fernet
+import re
 import asyncio
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix='/', intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 #telegram code 
 token = os.environ['token']
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
@@ -123,12 +126,31 @@ def remove_chat_id(message):
 #list 
 @bot.message_handler(commands=['list'])
 def get_chat_ids(message):
-		if str(message.from_user.id) not in trusted_users:
-				bot.reply_to(message, 'У вас нет прав для выполнения этой команды.')
-				return
+    if str(message.from_user.id) not in trusted_users:
+        bot.reply_to(message, 'У вас нет прав для выполнения этой команды.')
+        return
 
-		chat_ids = ', '.join(CHAT_ID)
-		bot.reply_to(message, f'Chat IDs: {chat_ids}')
+    # Create an inline keyboard
+    markup = types.InlineKeyboardMarkup()
+
+    # Add a button for each chat ID, allowing the administrator to choose which one to remove
+    for chat_id in CHAT_ID:
+        button = types.InlineKeyboardButton(f"Удалить {chat_id}", callback_data=f"delete_{chat_id}")
+        markup.add(button)
+
+    bot.reply_to(message, 'Выберите chat_id для удаления:', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('delete_'))
+def delete_chat_id(call):
+    chat_id_to_delete = call.data.split('_')[1]  # Extract the chat ID to be deleted from the callback data
+    if chat_id_to_delete in CHAT_ID:
+        CHAT_ID.remove(chat_id_to_delete)
+        bot.answer_callback_query(call.id, f'Chat ID {chat_id_to_delete} успешно удален.')
+    else:
+        bot.answer_callback_query(call.id, f'Chat ID {chat_id_to_delete} не найден.')
+
+    # Optionally, update the user interface with the modified list
+    # You can send an updated list or perform any other necessary action
 #add
 @bot.message_handler(commands=['add'])
 def add_chat_id(message):
@@ -147,55 +169,88 @@ def add_chat_id(message):
 				bot.reply_to(message, f'Chat ID {new_chat_id} успешно добавлен.')
 		else:
 				bot.reply_to(message, f'Chat ID {new_chat_id} уже существует.')
-#log 
-@bot.message_handler(commands=['log'])
-def send_log(message):
+
+#kick
+@bot.message_handler(commands=['kick'])
+def kick_from_voice_channel(message):
+		if str(message.from_user.id) not in trusted_users:
+				bot.reply_to(message, 'У вас нет прав для выполнения этой команды.')
+				return
+
+		# Создаем кнопки для каждого сервера
 		markup = types.InlineKeyboardMarkup()
-		markup.add(
-				types.InlineKeyboardButton("Сервер 1", callback_data='server1'),
-				types.InlineKeyboardButton("Сервер 2", callback_data='server2'),
-				# Добавьте больше кнопок, если нужно
-		)
+		for guild in client.guilds:
+				markup.add(types.InlineKeyboardButton(guild.name, callback_data=f'kick_guild_{guild.id}'))
+
 		bot.send_message(message.chat.id, "Выберите сервер:", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-		server_name = ''
-		if call.data == 'server1':
-				server_name = 'ITACHI TEST'
-				bot.answer_callback_query(call.id, 'Отправка логов с сервера 1...')
-		elif call.data == 'server2':
-				server_name = 'ANOTHER SERVER'
-				bot.answer_callback_query(call.id, 'Отправка логов с сервера 2...')
-		# Добавьте больше условий, если нужно
+		if call.data.startswith('kick_guild_'):
+				_, guild_id, *_ = call.data.split('_')
 
-		with open('log.txt', 'r') as file:
-				lines = file.readlines()
+				# Создаем кнопки для каждого голосового канала на сервере
+				markup = types.InlineKeyboardMarkup()
+				guild = client.get_guild(int(guild_id))
+				for channel in guild.voice_channels:
+						markup.add(types.InlineKeyboardButton(channel.name, callback_data=f'kick_channel_{guild_id}_{channel.id}'))
 
-		# Фильтруем строки по имени сервера
-		server_lines = [line for line in lines if line.startswith(f'[{server_name}]')]
+				bot.edit_message_text("Выберите голосовой канал:", call.message.chat.id, call.message.message_id, reply_markup=markup)
 
-		# Создаем новый файл с отфильтрованными строками
-		with open('filtered_log.txt', 'w') as file:
-				file.writelines(server_lines)
+		elif call.data.startswith('kick_channel_'):
+				_, guild_id, channel_id, *_ = call.data.split('_')
 
-		# Отправляем новый файл
-		with open('filtered_log.txt', 'rb') as file:
-				bot.send_document(call.message.chat.id, file)
+				# Создаем кнопки для каждого пользователя в голосовом канале
+				markup = types.InlineKeyboardMarkup()
+				channel = client.get_channel(int(channel_id))
+				for member in channel.members:
+						markup.add(types.InlineKeyboardButton(member.name, callback_data=f'kick_member_{guild_id}_{channel_id}_{member.id}'))
 
+				bot.edit_message_text("Выберите пользователя:", call.message.chat.id, call.message.message_id, reply_markup=markup)
 
+		elif call.data.startswith('kick_member_'):
+				_, guild_id, channel_id, user_id, *_ = call.data.split('_')
+
+				# Находим пользователя и голосовой канал
+				user = client.get_user(int(user_id))
+				channel = client.get_channel(int(channel_id))
+
+				if user and channel and isinstance(channel, discord.VoiceChannel):
+						# Если пользователь находится в голосовом канале, кикаем его
+						if user in channel.members:
+								asyncio.run_coroutine_threadsafe(user.move_to(None), client.loop)
+								bot.answer_callback_query(call.id, f'Пользователь {user_id} был кикнут из голосового канала {channel_id}.')
+						else:
+								bot.answer_callback_query(call.id, f'Пользователь {user_id} не находится в голосовом канале {channel_id}.')
+				else:
+						bot.answer_callback_query(call.id, 'Не удалось найти пользователя или голосовой канал.')
+#log 
+
+@bot.message_handler(commands=['log'])
+def send_file(message):
+		chat_id = str(message.chat.id)
+		if chat_id not in CHAT_ID:
+				bot.send_message(chat_id, 'Ваш chat_id не добавлен в список. Используйте команду /requestadd, чтобы запросить доступ.')
+				return
+
+		file_path = 'log.txt'
+		if os.path.getsize(file_path) <= 4096:  # 4 KB = 4096 bytes
+				bot.send_message(chat_id, "Лог еще пустой.")
+		else:
+				with open(file_path, 'rb') as file:
+						bot.send_document(chat_id, file)
 ## Обновленное событие для логирования входа в голосовой канал
 @client.event
 async def on_voice_state_update(member, before, after):
-		if before.channel != after.channel:  # Проверяем, изменился ли голосовой канал
-				if after.channel:  # Если член зашел в голосовой канал
-						log_message(f'{member} зашел в голосовой канал {after.channel} на сервере {after.channel.guild}.')
-						if member.bot:
-								return
-				else:  # Если член вышел из голосового канала
-						log_message(f'{member} покинул голосовой канал {before.channel} на сервере {before.channel.guild}.')
-						if member.bot:
-								return
+		if before.channel != after.channel:  # Check if the voice channel has changed
+				if after.channel:  # If the member entered a voice channel
+						if after.channel.guild:  # Ensure that the voice channel belongs to a guild
+								log_message(f'{after.channel.guild} - {member} вошел в голосовой канал {after.channel}.')
+				elif before.channel:  # If the member left the voice channel
+						if before.channel.guild:  # Ensure that the voice channel belonged to a guild
+								log_message(f'{before.channel.guild} - {member} вышел из голосового канала {before.channel}.')
+
+
 
 @client.event
 async def on_message(message):
@@ -205,7 +260,7 @@ async def on_message(message):
 @client.event
 async def on_member_update(before, after):
 		# Логируем изменения статуса участников
-		log_message(f'{after.guild} - {after.name} изменил статус: {before.status} -> {after.status}')
+		log_message(f'{after.guild} - {after.name} - изменил статус: {before.status} -> {after.status}')
 
 @client.event
 async def on_message_delete(message):
@@ -217,11 +272,15 @@ async def on_message_edit(before, after):
 		log_message(f'{before.guild} #{before.channel} - {before.author} изменил сообщение: {before.content} -> {after.content}')
 
 def log_message(text):
-		log_path = 'log.txt'
-		timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		log_entry = f'[{timestamp}] {text}\n'
-		with open(log_path, 'a', encoding='utf-8') as log_file:
-				log_file.write(log_entry)
+	log_path = 'log.txt'
+	moscow_tz = pytz.timezone('Europe/Moscow')  # Устанавливаем часовой пояс Москвы
+	moscow_time = datetime.now(moscow_tz)  # Получаем текущее время в Московском часовом поясе
+	timestamp = moscow_time.strftime("%Y-%m-%d %H:%M:%S")
+	log_entry = f'[{timestamp}] {text}\n'
+	with open(log_path, 'a', encoding='utf-8') as log_file:
+			log_file.write(log_entry)
+
+
 def start_polling():
 	bot.polling()
 
